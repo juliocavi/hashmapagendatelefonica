c5e6bc5e142b2c3abbea80c13372c1e26c0a2310
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2018-02-15 18:34:58 +0100

    Actividad 0089, implementados métodos con la clase hashmap

---
+++ AgendaTelefonica.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+/**
+ * Write a description of class AgendaTelefonica here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+    HashMap<String, String> agenda;
+
+    /**
+     * Constructor for objects of class AgendaTelefonica.
+     */
+    public AgendaTelefonica()
+    {
+        agenda = new HashMap<>();
+    }
+
+    /**
+     * Set the name and the number of a contact.
+     */
+    public void enterNumber(String name, String number)
+    {
+        agenda.put(name, number);
+    }
+
+    /**
+     * Get the number of a contact.
+     */
+    public String lookupNumber(String name)
+    {
+        String numeroDelContacto = agenda.get(name);
+        return numeroDelContacto;
+    }
+}
